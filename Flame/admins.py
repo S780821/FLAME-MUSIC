289d95ab0f78716edf6cc9e04dec41f54d05e74f
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-05-26 13:21:41 +0530

    Update admins.py

--- Flame/admins.py
+++ Flame/admins.py
@@ -1,6 +1,6 @@
 from typing import List
 from pyrogram.types import Chat
-from Zaid.Cache.admins import get as gett, set
+from Flame.Cache.admins import get as gett, set
 
 async def get_administrators(chat: Chat) -> List[int]:
     get = gett(chat.id)
