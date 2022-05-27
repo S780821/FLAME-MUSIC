d8a2284157e8c3c4f13fc5eadd8c81f1858620b8
Perfect255 <106339624+Perfect255@users.noreply.github.com>
2022-05-27 22:12:23 +0530

    Update gcast.py

--- Flame/Player/gcast.py
+++ Flame/Player/gcast.py
@@ -2,7 +2,7 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
-from modules.clientbot.clientbot import client as aditya
+from callsmusic.callsmusic import client as Bot
 from config import SUDO_USERS
 
 @Client.on_message(filters.command(["gcast", "broadcast"]))
