e4af30482b6c91b4e1f13dc476b65aa4314072fd
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-06-23 17:19:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,42 +9,17 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 admins = {}
-if  str(getenv("SESSION_NAME")).strip() == "":
-    SESSION_NAME = str(None)
-else:
-    SESSION_NAME = str(getenv("SESSION_NAME"))
-
-if str(getenv("STRING_SESSION2")).strip() == "":
-    SESSION2 = str(None)
-else:
-    SESSION2 = str(getenv("STRING_SESSION2"))
-
-if str(getenv("STRING_SESSION3")).strip() == "":
-    SESSION3 = str(None)
-else:
-    SESSION3 = str(getenv("STRING_SESSION3"))
-
-if str(getenv("STRING_SESSION4")).strip() == "":
-    SESSION4 = str(None)
-else:
-    SESSION4 = str(getenv("STRING_SESSION4"))
-
-if str(getenv("STRING_SESSION5")).strip() == "":
-    SESSION5 = str(None)
-else:
-    SESSION5 = str(getenv("STRING_SESSION5"))
-
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_NAME = getenv("BOT_NAME", "Umk")
-API_ID = int(getenv("API_ID", "8187557"))
-API_HASH = getenv("API_HASH", "efd77b34c69c164ce1587ff5a0d117")
-OWNER_NAME = getenv("OWNER_NAME", "Zaid")
+SESSION_NAME = getenv("SESSION_NAME", "BQAHQiw0QYyBC1Z27RNxDUExj0wm-ibSuWlXWTNb65I4PyzUD8QvC5TqVPdbmcNk0dIKy5TVzb3yGNx3asxVw-2wj9euifBlnWVWZ4CIdsffEA2h0HgxF40k66PJA_xS3bbpJAw--8Yrrjtz362FzKwCmuoxcKd8onklWX9fBvGhJ99nNEq8lpNCRsxEpmw7qr8gOw4b503HHXHnU9IZEmFCmY2CC47-Ec8HzCCXOxBEJEnOoA9Gf-431tOOn_13c9QF7AI1oHxUC5-5HQyxCjnyK8SuofRX8gjBbtze6xfIMBFp9NH6ev4i1GTjy9BNGKy8C-WBgV2pI-pOw3pttpmZAAAAATUTEHYA")
+BOT_TOKEN = getenv("BOT_TOKEN", "5300563520:AAGcZ1kQeipKuu3rAW7I7SvmuAx0f67YDN4")
+BOT_NAME = getenv("BOT_NAME", "Flamecircle_bot")
+API_ID = int(getenv("API_ID", "8201417"))
+API_HASH = getenv("API_HASH", "4de3ab03e330698fc1a8fbf2c85b3997")
+OWNER_NAME = getenv("OWNER_NAME", "Perfect")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "Xmartperson")
 ALIVE_NAME = getenv("ALIVE_NAME", "Flame")
-MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Cloner:Cloner@cluster0.cgc6t.mongodb.net/?retryWrites=true&w=majority")
-BOT_USERNAME = getenv("BOT_USERNAME", "Flamemusicbot")
+BOT_USERNAME = getenv("BOT_USERNAME", "Flamecircle_bot")
 OWNER_ID = getenv("OWNER_ID", "5083524212")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Flame_assistant")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Checkbt5")
 GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Flame_project")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Flame_Updates")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
