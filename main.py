59a5a085093d9f7fceb1013e999b67101e0d2131
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-06-23 07:18:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import asyncio
 from pytgcalls import idle
-from Flame.main import call_py, bot, BOT
+from Flame.Database import db
 
-async def start_bot():
-    print("[INFO]: STARTING BOT CLIENT")
-    await bot.start()
-    print("[INFO]: STARTING PYTGCALLSS CLIENT")
-    await call_py.start()
-    await idle()
-    print("[INFO]: STOPPING BOT & USERBOT")
-    await bot.stop()
+import os
+import sys
+import random
+import asyncio
+from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME, SESSION2
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+from Flame.main import *
+
 
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(start_bot())
-BOT.run_until_disconnected()
